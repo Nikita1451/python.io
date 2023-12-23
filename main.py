1a9aac28df42866a86cfdd5f72a9b44229313e7f
Яндекс Лицей 2022 <nikijakushev@yandex.ru>
2023-12-23 16:56:43 +0300

    w

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import sys
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Suprematism(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)
+        self.pushButton.clicked.connect(self.draw)
+    def draw(self):
+        self.update()
+
+    def paintEvent(self, event):
+        w = randint(0, 200)
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QColor(0, 255, 255))
+        qp.drawEllipse(200, 200, w, w)
+        qp.drawEllipse(400, 400, w, w)
+        qp.end()
+
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.exit(app.exec())
